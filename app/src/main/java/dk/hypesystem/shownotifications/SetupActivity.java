f1d9f4ff557afa642c341b8f2fdb828eea807956
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-07-19 22:03:45 +0200

    Added SetupActivity outline

---
+++ app/src/main/java/dk/hypesystem/shownotifications/SetupActivity.java
@@ -0,0 +1,12 @@
+package dk.hypesystem.shownotifications;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_activity);
+    }
+}
